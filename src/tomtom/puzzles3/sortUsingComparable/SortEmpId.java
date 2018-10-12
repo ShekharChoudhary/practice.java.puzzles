24acab05046ec2e5c39b9e2c4055c7ef7ea9408b
Choudhary Shekhar <shekhar.choudhary@tieto.com>
2018-10-12 16:52:24 +0530

    adding changes

--- src/tomtom/puzzles3/sortUsingComparable/SortEmpId.java
+++ src/tomtom/puzzles3/sortUsingComparable/SortEmpId.java
@@ -7,14 +7,13 @@ public class SortEmpId implements Comparator{
 	
 	public int compare(Object o1, Object o2) {
 		
-		SortUsingComparable bvalue = new SortUsingComparable();
 		
 		Employee e1 = (Employee) o1;
 		Employee e2 = (Employee) o2;
 		
-		if(bvalue.baseList.indexOf(e1.id) > bvalue.baseList.indexOf(e2.id))
+		if(SortUsingComparable.baseList.indexOf(e1.id) > SortUsingComparable.baseList.indexOf(e2.id))
 		return 1;
-		else if(bvalue.baseList.indexOf(e1.id) < bvalue.baseList.indexOf(e2.id))
+		else if(SortUsingComparable.baseList.indexOf(e1.id) < SortUsingComparable.baseList.indexOf(e2.id))
 			return -1;
 		else 
 			return 0;
